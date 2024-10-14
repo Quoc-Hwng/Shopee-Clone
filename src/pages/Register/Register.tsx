67d98da2890fc927d24e8a823a1b7b2cfd4dec6b
Quoc-Hwng <phamhung151020@gmail.com>
2024-10-14 16:56:33 +0700

    register-ui-and-login-ui

--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -1,3 +1,52 @@
+import { Link } from 'react-router-dom'
+
 export default function Register() {
-  return <div>register</div>
+  return (
+    <div className='bg-orange'>
+      <div className='max-w-7xl mx-auto px-4'>
+        <div className='grid grid-cols-1 lg:grid-cols-5 lg:py-32 lg:pr-10'>
+          <div className='lg:col-span-2 lg:col-start-4'>
+            <div className='p-10 rounded bg-white shadow-sm'>
+              <div className='text-2xl'>Đăng ký </div>
+              <div className='mt-8'>
+                <input
+                  type='email'
+                  name='email'
+                  className='p-3 w-full outline-none border border-gray-300 focus:border-gray-500 rounded-sm focus:shadow-sm'
+                />
+                <div className='mt-1 text-red-600 text-sm min-h-[1rem]'>Email không hợp lệ</div>
+              </div>
+              <div className='mt-1'>
+                <input
+                  type='password'
+                  name='password'
+                  className='p-3 w-full outline-none border border-gray-300 focus:border-gray-500 rounded-sm focus:shadow-sm'
+                />
+                <div className='mt-1 text-red-600 text-sm min-h-[1rem]'>Password không hợp lệ</div>
+              </div>
+              <div className='mt-1'>
+                <input
+                  type='password'
+                  name='confirm_password'
+                  className='p-3 w-full outline-none border border-gray-300 focus:border-gray-500 rounded-sm focus:shadow-sm'
+                />
+                <div className='mt-1 text-red-600 text-sm min-h-[1rem]'>Password không hợp lệ</div>
+              </div>
+              <div className='mt-3'>
+                <button className='w-full text-center py-4 px-2 uppercase bg-red-500 text-white text-sm hover:bg-red-600'>
+                  Đăng ký
+                </button>
+              </div>
+              <div className='flex items-center justify-center mt-8'>
+                <span className='text-slate-400'>Bạn đã có tài khoản?</span>
+                <Link className='text-red-400 ml-1' to='/login'>
+                  Đăng nhập
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
