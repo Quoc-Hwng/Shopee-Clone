c33317be24017aae0d5aec834cdfcb125a1de528
Quoc-Hwng <phamhung151020@gmail.com>
2024-10-16 11:26:26 +0700

    validate-form-with-react-hook-form

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,28 +1,44 @@
+import { useForm } from 'react-hook-form'
 import { Link } from 'react-router-dom'
+import { getRules } from '../../utils/rules'
+interface FormData {
+  email: string
+  password: string
+}
 
 export default function Login() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm<FormData>()
+  const rules = getRules()
+  const onsubmit = handleSubmit((data) => {
+    console.log(data)
+  })
   return (
     <div className='bg-orange'>
       <div className='max-w-7xl mx-auto px-4'>
         <div className='grid grid-cols-1 lg:grid-cols-5 lg:py-32 lg:pr-10'>
           <div className='lg:col-span-2 lg:col-start-4'>
-            <div className='p-10 rounded bg-white shadow-sm'>
+            <form className='p-10 rounded bg-white shadow-sm' onSubmit={onsubmit}>
               <div className='text-2xl'>Đăng nhập</div>
               <div className='mt-8'>
                 <input
                   type='email'
-                  name='email'
                   className='p-3 w-full outline-none border border-gray-300 focus:border-gray-500 rounded-sm focus:shadow-sm'
+                  {...register('email', { ...rules.email, deps: ['password'] })}
                 />
-                <div className='mt-1 text-red-600 text-sm min-h-[1rem]'>Email không hợp lệ</div>
+                <div className='mt-1 text-red-600 text-sm min-h-[1.25rem]'>{errors.email?.message}</div>
               </div>
               <div className='mt-1'>
                 <input
                   type='password'
-                  name='password'
+                  autoComplete='on'
+                  {...register('password', { ...rules.password, deps: ['email'] })}
                   className='p-3 w-full outline-none border border-gray-300 focus:border-gray-500 rounded-sm focus:shadow-sm'
                 />
-                <div className='mt-1 text-red-600 text-sm min-h-[1rem]'>Password không hợp lệ</div>
+                <div className='mt-1 text-red-600 text-sm min-h-[1.25rem]'>{errors.password?.message}</div>
               </div>
               <div className='mt-3'>
                 <button className='w-full text-center py-4 px-2 uppercase bg-red-500 text-white text-sm hover:bg-red-600'>
@@ -35,7 +51,7 @@ export default function Login() {
                   Đăng ký
                 </Link>
               </div>
-            </div>
+            </form>
           </div>
         </div>
       </div>
--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -1,36 +1,59 @@
+import { useForm } from 'react-hook-form'
 import { Link } from 'react-router-dom'
+import { getRules } from '../../utils/rules'
+
+interface FormData {
+  email: string
+  password: string
+  confirm_password: string
+}
 
 export default function Register() {
+  const {
+    register,
+    handleSubmit,
+    getValues,
+    formState: { errors }
+  } = useForm<FormData>()
+  const rules = getRules(getValues)
+  const onSubmit = handleSubmit((data) => {
+    console.log(data)
+  })
   return (
     <div className='bg-orange'>
       <div className='max-w-7xl mx-auto px-4'>
         <div className='grid grid-cols-1 lg:grid-cols-5 lg:py-32 lg:pr-10'>
           <div className='lg:col-span-2 lg:col-start-4'>
-            <div className='p-10 rounded bg-white shadow-sm'>
+            <form className='p-10 rounded bg-white shadow-sm' onSubmit={onSubmit} noValidate>
               <div className='text-2xl'>Đăng ký </div>
               <div className='mt-8'>
                 <input
                   type='email'
-                  name='email'
                   className='p-3 w-full outline-none border border-gray-300 focus:border-gray-500 rounded-sm focus:shadow-sm'
+                  placeholder='Email'
+                  {...register('email', { ...rules.email, deps: ['password', 'confirm_password'] })}
                 />
-                <div className='mt-1 text-red-600 text-sm min-h-[1rem]'>Email không hợp lệ</div>
+                <div className='mt-1 text-red-600 text-sm min-h-[1.25rem]'>{errors.email?.message}</div>
               </div>
               <div className='mt-1'>
                 <input
                   type='password'
-                  name='password'
                   className='p-3 w-full outline-none border border-gray-300 focus:border-gray-500 rounded-sm focus:shadow-sm'
+                  placeholder='Password'
+                  autoComplete='on'
+                  {...register('password', { ...rules.password, deps: ['email', 'confirm_password'] })}
                 />
-                <div className='mt-1 text-red-600 text-sm min-h-[1rem]'>Password không hợp lệ</div>
+                <div className='mt-1 text-red-600 text-sm min-h-[1.25rem]'>{errors.password?.message}</div>
               </div>
               <div className='mt-1'>
                 <input
                   type='password'
-                  name='confirm_password'
                   className='p-3 w-full outline-none border border-gray-300 focus:border-gray-500 rounded-sm focus:shadow-sm'
+                  placeholder='Confirm Password'
+                  autoComplete='on'
+                  {...register('confirm_password', { ...rules.confirm_password, deps: ['email', 'password'] })}
                 />
-                <div className='mt-1 text-red-600 text-sm min-h-[1rem]'>Password không hợp lệ</div>
+                <div className='mt-1 text-red-600 text-sm min-h-[1.25rem]'>{errors.confirm_password?.message}</div>
               </div>
               <div className='mt-3'>
                 <button className='w-full text-center py-4 px-2 uppercase bg-red-500 text-white text-sm hover:bg-red-600'>
@@ -43,7 +66,7 @@ export default function Register() {
                   Đăng nhập
                 </Link>
               </div>
-            </div>
+            </form>
           </div>
         </div>
       </div>
