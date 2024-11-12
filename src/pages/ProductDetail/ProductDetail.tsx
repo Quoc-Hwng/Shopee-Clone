405d145338f3879f722689bdf7fccb2b7f3a3907
Quoc-Hwng <phamhung151020@gmail.com>
2024-11-12 11:38:41 +0700

    slider-product-images

--- src/pages/ProductDetail/ProductDetail.tsx
+++ src/pages/ProductDetail/ProductDetail.tsx
@@ -5,6 +5,8 @@ import ProductRating from '../../components/ProductRating'
 import { formatCurrency, formatNumberToSocialStyle, rateSale } from '../../utils/utils'
 import InputNumber from '../../components/InputNumber'
 import DOMPurify from 'dompurify'
+import { useEffect, useMemo, useState } from 'react'
+import { Product } from '../../types/product.type'
 
 export default function ProductDetail() {
   const { id } = useParams()
@@ -12,22 +14,52 @@ export default function ProductDetail() {
     queryKey: ['product', id],
     queryFn: () => productApi.getProductDetail(id as string)
   })
+
+  const [currentIndexImages, setCurrentIndexImages] = useState([0, 5])
+  const [activeImage, setActiveImage] = useState('')
   const product = productDetailData?.data.data
+  const currentImages = useMemo(
+    () => (product ? product.images.slice(...currentIndexImages) : []),
+    [product, currentIndexImages]
+  )
+
+  useEffect(() => {
+    if (product && product.images.length > 0) {
+      setActiveImage(product.images[0])
+    }
+  }, [product])
+
+  const next = () => {
+    if (currentIndexImages[1] < (product as Product).images.length) {
+      setCurrentIndexImages((prev) => [prev[0] + 1, prev[1] + 1])
+    }
+  }
+  const prev = () => {
+    if (currentIndexImages[0] > 0) {
+      setCurrentIndexImages((prev) => [prev[0] - 1, prev[1] - 1])
+    }
+  }
+
+  const chooseActive = (img: string) => {
+    setActiveImage(img)
+  }
+
+  if (!product) return null
   return (
     <div className='bg-gray-200 py-6'>
-      <div className='bg-white p-4 shadow'>
-        <div className='container'>
+      <div className='container'>
+        <div className='bg-white p-4 shadow'>
           <div className='grid grid-cols-12 gap-9'>
             <div className='col-span-5'>
               <div className='relative w-full pt-[100%] shadow'>
                 <img
-                  src={product?.image}
-                  alt={product?.name}
+                  src={activeImage}
+                  alt={product.name}
                   className='absolute top-0 left-0 h-full w-full bg-white object-cover'
                 />
               </div>
               <div className='relative mt-4 grid grid-cols-5 gap-1'>
-                <button className='absolute left-0 top-1/2 z-10 h-9 w-5 -translate-y-1/2'>
+                <button className='absolute left-0 top-1/2 z-10 h-9 w-5 -translate-y-1/2' onClick={prev}>
                   <svg
                     xmlns='http://www.w3.org/2000/svg'
                     fill='none'
@@ -39,10 +71,10 @@ export default function ProductDetail() {
                     <path strokeLinecap='round' strokeLinejoin='round' d='M15.75 19.5 8.25 12l7.5-7.5' />
                   </svg>
                 </button>
-                {product?.images.slice(0, 5).map((img, index) => {
-                  const isActive = index === 0
+                {currentImages.map((img) => {
+                  const isActive = img === activeImage
                   return (
-                    <div className='relative w-full pt-[100%]' key={img}>
+                    <div className='relative w-full pt-[100%]' key={img} onMouseEnter={() => chooseActive(img)}>
                       <img
                         src={img}
                         alt={product?.name}
@@ -52,7 +84,7 @@ export default function ProductDetail() {
                     </div>
                   )
                 })}
-                <button className='absolute right-0 top-1/2 z-10 h-9 w-5 -translate-y-1/2'>
+                <button className='absolute right-0 top-1/2 z-10 h-9 w-5 -translate-y-1/2' onClick={next}>
                   <svg
                     xmlns='http://www.w3.org/2000/svg'
                     fill='none'
@@ -67,27 +99,27 @@ export default function ProductDetail() {
               </div>
             </div>
             <div className='col-span-7'>
-              <h1 className='text-xl font-medium uppercase'>{product?.name}</h1>
+              <h1 className='text-xl font-medium uppercase'>{product.name}</h1>
               <div className='mt-8 flex items-center'>
                 <div className='flex items-center'>
-                  <span className='mr-1 border-b border-b-orange text-orange'>{product?.rating}</span>
+                  <span className='mr-1 border-b border-b-orange text-orange'>{product.rating}</span>
                   <ProductRating
-                    rating={product?.rating ?? 0}
+                    rating={product.rating}
                     activeClassname='fill-orange text-orange h-4 w-4'
                     nonActiveClassname='fill-gray-300 text-gray-300 h-4 w-4'
                   />
                 </div>
                 <div className='mx-4 h-4 w-[1px] bg-gray-300'></div>
                 <div>
-                  <span>{formatNumberToSocialStyle(product?.sold ?? 0)}</span>
+                  <span>{formatNumberToSocialStyle(product.sold)}</span>
                   <span className='ml-1 text-gray-500'>Đã bán</span>
                 </div>
               </div>
               <div className='mt-8 flex items-center bg-gray-50 px-5 py-4'>
-                <div className='text-gray-500 line-through'>₫{formatCurrency(product?.price_before_discount ?? 0)}</div>
-                <div className='ml-3 text-3xl font-medium text-orange'>₫{formatCurrency(product?.price ?? 0)}</div>
+                <div className='text-gray-500 line-through'>₫{formatCurrency(product.price_before_discount)}</div>
+                <div className='ml-3 text-3xl font-medium text-orange'>₫{formatCurrency(product.price)}</div>
                 <div className='ml-4 rounded-sm bg-orange px-1 py-[2px] text-xs font-semibold uppercase text-white'>
-                  {rateSale(product?.price_before_discount ?? 0, product?.price ?? 0)} giảm
+                  {rateSale(product.price_before_discount, product.price)} giảm
                 </div>
               </div>
               <div className='mt-8 flex items-center'>
@@ -124,7 +156,7 @@ export default function ProductDetail() {
                     </svg>
                   </button>
                 </div>
-                <div className='ml-6 text-sm text-gray-500'>{product?.quantity} sản phẩm có sẵn</div>
+                <div className='ml-6 text-sm text-gray-500'>{product.quantity} sản phẩm có sẵn</div>
               </div>
               <div className='mt-8 flex items-center'>
                 <button className='flex h-12 items-center justify-center rounded-sm border border-orange bg-orange/10 px-5 capitalize text-orange shadow-sm hover:bg-orange/5'>
@@ -161,13 +193,13 @@ export default function ProductDetail() {
           </div>
         </div>
       </div>
-      <div className='mt-8 bg-white p-4 shadow'>
-        <div className='container'>
+      <div className='container'>
+        <div className='mt-8 bg-white p-4 shadow'>
           <div className='rounded bg-gray-50 p-4 text-lg capitalize text-slate-700'>MÔ TẢ SẢN PHẨM</div>
           <div className='mx-4 mt-12 mb-4 text-sm loading-loose'>
             <div
               dangerouslySetInnerHTML={{
-                __html: DOMPurify.sanitize(product?.description ?? '')
+                __html: DOMPurify.sanitize(product.description)
               }}
             />
           </div>
