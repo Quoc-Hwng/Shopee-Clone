af7109293b1caed9f5ec00b464225c94f818d1b2
Quoc-Hwng <phamhung151020@gmail.com>
2024-11-15 09:53:23 +0700

    update-code-inputNumber-and-quantityController-with-local-state-component

--- src/components/QuantityController/QuantityController.tsx
+++ src/components/QuantityController/QuantityController.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import InputNumber, { InputNumberProps } from '../InputNumber'
 
 interface QuantityControllerProps extends InputNumberProps {
@@ -17,6 +18,7 @@ export default function QuantityController({
   value,
   ...rest
 }: QuantityControllerProps) {
+  const [localValue, setLocalValue] = useState<number>(Number(value || 1))
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     let _value = Number(e.target.value)
     if (max !== undefined && _value > max) {
@@ -27,25 +29,28 @@ export default function QuantityController({
     if (onType) {
       onType(_value)
     }
+    setLocalValue(_value)
   }
 
   const handleIncrease = () => {
-    let _value = Number(value) + 1
+    let _value = Number(value || localValue) + 1
     if (max !== undefined && _value >= max) {
       _value = max
     }
     if (onIncrease) {
       onIncrease(_value)
     }
+    setLocalValue(_value)
   }
   const handleDecrease = () => {
-    let _value = Number(value) - 1
+    let _value = Number(value || localValue) - 1
     if (_value < 1) {
       _value = 1
     }
     if (onDecrease) {
       onDecrease(_value)
     }
+    setLocalValue(_value)
   }
 
   return (
@@ -66,7 +71,7 @@ export default function QuantityController({
         </svg>
       </button>
       <InputNumber
-        value={value}
+        value={value || localValue}
         className=''
         classNameError='hidden'
         classNameInput='h-8 w-14 border-t border-b border-gray-300 p-1 text-center outline-none'
